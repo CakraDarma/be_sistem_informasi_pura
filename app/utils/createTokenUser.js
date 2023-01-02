0ab613b5e259da0836ad8aed7bb3f1a22734c985
CakraDarma <baguscakraa02@gmail.com>
2023-01-02 11:26:35 +0800

    template project

---
+++ app/utils/createTokenUser.js
@@ -0,0 +1,22 @@
+// fungsinya nampung kedalam object setelah di findone
+
+const createTokenUser = (user) => {
+	return {
+		name: user.name,
+		userId: user._id,
+		role: user.role,
+		email: user.email,
+		organizer: user.organizer,
+	};
+};
+
+const createTokenParticipant = (participant) => {
+	return {
+		lastName: participant.lastName,
+		participantId: participant._id,
+		firstName: participant.firstName,
+		email: participant.email,
+	};
+};
+
+module.exports = { createTokenUser, createTokenParticipant };
