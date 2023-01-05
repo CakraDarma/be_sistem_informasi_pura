1e6b099ed0e04f2aac6b74a0d3d6540e63af8e2e
CakraDarma <baguscakraa02@gmail.com>
2023-01-06 04:42:52 +0800

    add custom validation

--- app/api/v1/products/validator.js
+++ app/api/v1/products/validator.js
@@ -1,8 +1,11 @@
 const Joi = require('joi');
+const message = require('../../../utils/customValidation.js');
+
+const { string, number } = message;
 
 const schema = Joi.object({
-	name: Joi.string().alphanum().min(3).max(30).required(),
-	price: Joi.number().integer().min(1900).max(2013),
+	name: Joi.string().min(2).max(10).required().messages(string),
+	price: Joi.number().integer().min(19).max(2013).message(number),
 });
 
 module.exports = {
